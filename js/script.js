2ba117ff67552f06893434c4e7da8f5aa1cfbf51
CunningCrest <artem.gorbatenko.98@mail.ru>
2018-01-27 11:09:15 +0300

    first

---
+++ js/script.js
@@ -0,0 +1,54 @@
+localStorage.clear();
+if (localStorage.length != 0) {
+  alert("Зашел в if");
+  var model = JSON.parse(localStorage.getItem("key"));
+} else {
+  alert("Зашел в else");
+  var model = {
+    items: [{
+        description: "Купить хлеб",
+      },
+      {
+        description: "Заплатить кредит",
+      },
+      {
+        description: "Посетить врача",
+      },
+      {
+        description: "Съездить к другу",
+      }
+    ]
+  };
+  localStorage.setItem("key", JSON.stringify(model));
+}
+var taskListApp = angular.module("taskListApp", []);
+taskListApp.controller("taskListController", function($scope) {
+  var tempIndex;
+  $scope.list = model;
+  $scope.addItem = function(text) {
+    if (text != "") {
+      $scope.list.items.push({
+        description: text,
+      });
+      model = $scope.list;
+      localStorage.setItem("key", JSON.stringify(model));
+    }
+  }
+  $scope.removeItem = function(deleteIndex) {
+    $scope.list.items.splice(deleteIndex, 1);
+    model = $scope.list;
+    localStorage.setItem("key", JSON.stringify(model));
+  }
+  $scope.editItem = function(editIndex) {
+    document.getElementById('inputElement').setAttribute("value",
+      $scope.list.items[editIndex].description);
+    tempIndex = editIndex;
+  }
+  $scope.saveChanges = function(text) {
+    $scope.list.items[tempIndex].description = text;
+    model = $scope.list;
+    localStorage.setItem("key", JSON.stringify(model));
+    document.getElementById('inputElement').value = "";
+  }
+
+});
